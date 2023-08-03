0c598fdf2817376dad40702f2af69309c0333968
brnydr <brianwilliamyoder@gmail.com>
2023-08-02 17:47:48 -0700

    add async

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,16 +1,16 @@
 export default class WeatherService {  
-  static getWeather(city) {
-   return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-   .then((response)=> {
-    if(!response.ok) {
-      const errorMessage = `${response.status} ${response.statusText}`;
-      throw new Error (errorMessage)
-    } else {
-      return response.json();
+  static async getWeather(city) {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+      const jsonifiedResponse = await response.json();
+      if(!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        throw new Error (errorMessage)
+      } 
+      return jsonifiedResponse;
     }
-   })
-   .catch((error) => {
+    catch(error) {
     return error;
-   });
+    }
   }
 }
