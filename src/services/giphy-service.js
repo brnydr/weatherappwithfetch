49e51980e4b88f015c82c1446a144ef2ffbccc17
brnydr <brianwilliamyoder@gmail.com>
2023-08-05 14:51:11 -0700

    fix spacing

--- src/services/giphy-service.js
+++ src/services/giphy-service.js
@@ -2,11 +2,11 @@ export default class GiphyService {
   static async getGif(input) {
     try {
       const response = fetch(`http://api.giphy.com/v1/gifs/search?q=${input}&api_key=${process.env.GIPHY_API_KEY}&limit=5`);
-      const jsonResponse = await response.json;
       if(!response.ok) {
         const err = `${response.status} ${response.statusText}`;
         throw new Error(err);
       }
+        const jsonResponse = await response.json;
         return jsonResponse;
       }
       catch (err) {
--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -1,16 +1,17 @@
 export default class WeatherService {  
   static async getWeather(city) {
     try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-      const jsonResponse = await response.json();
-      if(!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
-        throw new Error (errorMessage);
-      } 
-      return jsonResponse;
-    }
-    catch(error) {
-      return error;
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`);
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        throw new Error(errorMessage);
+      }
+      const weatherData = await response.json();
+      return weatherData;
+    } 
+    catch (err) {
+      return err;
     }
   }
 }
+
