44d0e84a95c4afdb628e7bcf81c66f44948f94f1
brnydr <brianwilliamyoder@gmail.com>
2023-08-05 14:19:13 -0700

    variable renaming

--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -2,12 +2,12 @@ export default class WeatherService {
   static async getWeather(city) {
     try {
       const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-      const jsonifiedResponse = await response.json();
+      const jsonResponse = await response.json();
       if(!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
         throw new Error (errorMessage);
       } 
-      return jsonifiedResponse;
+      return jsonResponse;
     }
     catch(error) {
       return error;
